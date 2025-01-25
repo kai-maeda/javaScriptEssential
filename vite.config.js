aa455c0d675bdc2f1ef815237ee720a7dcf96e89
Kai Maeda <kaimaeda@ucsb.edu>
2025-01-24 22:27:47 -0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/javaScriptEssentials",
+  base: "/javaScriptEssential",
   plugins: [react()],
 })
